f668d7c8d6608f5693f87652e045527910ea861a
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-01-25 14:35:04 +0100

    add test payEmployeesShouldNotPayEmployeeInCaseOfRuntimeException

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -11,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,4 +43,26 @@ class EmployeesTest {
         assertThat(payments).isEqualTo(3);
 
     }
+
+    @Test
+    @DisplayName("payEmployees should not pay employee in case of runtime exception")
+    void payEmployeesShouldNotPayEmployeeInCaseOfRuntimeException() {
+
+            DummyBankService dummyBankService = new DummyBankService() {
+
+                @Override
+                public void pay(String id, double amount) {
+                    if (id.equals("2"))
+                        throw new RuntimeException();
+                }
+            };
+            Mockito.when(employeeRepository.findAll()).thenReturn(List.of(new Employee("1", 1000), new Employee("2", 2000), new Employee("3", 3000)));
+            Employees employees = new Employees(employeeRepository, dummyBankService);
+
+            int payments = employees.payEmployees();
+
+            assertThat(payments).isEqualTo(2);
+
+    }
+
 }
