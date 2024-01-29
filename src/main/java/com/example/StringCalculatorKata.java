5a27e4de33200d37fe03e6a997423a8ae2ca9d91
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-01-29 15:55:39 +0100

    Update method addNumbers

--- src/main/java/com/example/StringCalculatorKata.java
+++ src/main/java/com/example/StringCalculatorKata.java
@@ -14,20 +14,30 @@ public class StringCalculatorKata {
             if (numbers.isEmpty())
                 return "0";
 
+            Pattern pattern = Pattern.compile("^//(?:\\[(.+?)]|(.+?))\n");
 
-            Pattern pattern = Pattern.compile("^//\\[(.+?)]\\n");
+
             Matcher matcher = pattern.matcher(numbers);
 
             String delimiter;
 
+            String[] numbersArray;
+
             if (matcher.find()) {
-                delimiter = matcher.group(1);
+
+                if (matcher.group(1) != null)
+                    delimiter = matcher.group(1);
+
+                else
+                    delimiter = matcher.group(2);
+
                 numbers = numbers.substring(matcher.end());
+                numbersArray = numbers.split(Pattern.quote(delimiter));
             } else {
-                delimiter = "[,\n]";
+                delimiter = "[,\n]"; //default delimiter
+                numbersArray = numbers.split(delimiter);
             }
 
-            String[] numbersArray = numbers.split(Pattern.quote(delimiter));
             List<Integer> numbersList = new ArrayList<>();
 
             for (String number : numbersArray) {
